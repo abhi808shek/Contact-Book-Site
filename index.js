59755cff78b4e513f620b657c9e917f3084aca68
Abhishek gupta <abhi808gupta@gmail.com>
2022-07-11 21:38:54 +0530

    Backend Updated

--- index.js
+++ index.js
@@ -15,10 +15,10 @@ const PORT = process.env.PORT || 8000;
 
 if(process.env.NODE_ENV === 'production'){
     //set static folder
-    app.use(express.static('client/build'));
+    app.use(express.static('/frontend/build'));
 }
 app.get('*',(req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
 });
 
 
